acde6c7236bd4781b84980326e66eb5dd794bb83
Yassir Taousi <yassir.taous@gmail.com>
2021-02-21 12:22:50 +0100

    need to fix ip redirection

--- K8/srcs/mysql/setup.sh
+++ K8/srcs/mysql/setup.sh
@@ -3,8 +3,16 @@ chown -R 777 /var/lib/mysql
 rc-update add mariadb
 rc-service mariadb restart
 
-#mysql < create_users.sql -u root -proot
-#mysql < wp_sql
+mysql < create_users.sql -u root -proot
+#mysql < wp_sql.sql -u root -proot
+
+
+rc-service mariadb stop
+mariadbd --user=root 
+#exec /bin/sh
+
+
 #start deamon /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
 #test deamon for file cd '/usr/mysql-test' ; perl mysql-test-run.pl
-exec /bin/sh
+
+
--- K8/srcs/wordpress/setup.sh
+++ K8/srcs/wordpress/setup.sh
@@ -32,4 +32,7 @@ sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo= ${PHP_CGI_FIX_PATHINFO}|i" /et
 
 rc-service nginx start
 rc-service php-fpm7 start
-exec /bin/sh
+
+nginx -s stop
+nginx -g 'deamon off;'
+#exec /bin/sh
